a8ceb68e8becf6901a8b61cf959a7376ad1d9c31
keivan85 <keyvankarimi.kk@gmail.com>
2020-12-13 19:54:12 +0200

    Add from form validation checks

--- includes/Forms.php
+++ includes/Forms.php
@@ -49,4 +49,103 @@ class Forms
         <div id="errorMessage">' . $ErrorMessage . '</div>
         </div>';
     }
+
+    //Create Email
+    public function makeInputEmail($LabelText, $ControlName)
+    {
+        //The stickyData is empty at the begining
+        $StickyValue = "";
+
+        if (isset($this->StickyData[$ControlName])) {
+            $StickyData = $this->StickyData[$ControlName];
+        }
+
+        //Error message will be empty in the begining
+        $ErrorMessage = "";
+
+        if (isset($this->ValidationError[$ControlName])) {
+            $ErrorMessage = $this->ValidationError[$ControlName];
+        }
+
+        //Create html
+        $this->HTML .= '<div class="form-group"> 
+        <label for="' . $ControlName . '">' . $LabelText . '</label>
+        <input type = "email" name="' . $ControlName . '"class="form-control value"' . $StickyValue . '" placeholder = "Enter ' . $ControlName . '">
+        <div id="errorMessage">' . $ErrorMessage . '</div>
+        </div>';
+    }
+
+    //Create Password
+    public function makePassword($LabelText, $ControlName)
+    {
+        //The stickyData is empty at the begining
+        $StickyValue = "";
+
+        if (isset($this->StickyData[$ControlName])) {
+            $StickyData = $this->StickyData[$ControlName];
+        }
+
+        //Error message will be empty in the begining
+        $ErrorMessage = "";
+
+        if (isset($this->ValidationError[$ControlName])) {
+            $ErrorMessage = $this->ValidationError[$ControlName];
+        }
+
+        //Create html
+        $this->HTML .= '<div class="form-group"> 
+            <label for="' . $ControlName . '">' . $LabelText . '</label>
+            <input type = "password" name="' . $ControlName . '"class="form-control value"' . $StickyValue . '" placeholder = "Enter ' . $ControlName . '">
+            <div id="errorMessage">' . $ErrorMessage . '</div>
+            </div>';
+    }
+
+    //Create submit
+    public function makeSubmit($ControlName)
+    {
+
+        $this->HTML .= '<input type="submit" class="btn btn-primary" name="' . $ControlName . '" value="' . $ControlName . '">';
+    }
+
+    //Validation methods
+    public function checkEmpty($ControlName)
+    {
+
+        $Value = "";
+        if (isset($this->StickyData[$ControlName]) == TRUE) {
+            $Value = $this->StickyData[$ControlName];
+        } else {
+            $this->ValidationError[$ControlName] = 'Must not be empty';
+        }
+    }
+
+    //Compare two values
+    public function compare($Control1, $Control2)
+    {
+
+        if ($this->StickyData[$Control1] != $this->StickyData[$Control2]) {
+
+            $this->ValidationError[$Control2] = 'Values are not equal';
+        }
+    }
+
+    //Check email has valid format
+    public function checkEmail($ControlName)
+    {
+
+        $Value = '';
+        if (isset($this->StickyData[$ControlName]) == TRUE) {
+            $Value = $this->StickyData[$ControlName];
+        }
+
+        if (!filter_var($Value, FILTER_VALIDATE_EMAIL)) {
+            $this->ValidationError[$ControlName] = 'The meail format is not valid';
+        }
+    }
+
+    //Raise custom errors
+    public function raiseCustomError($Control, $ErrorMessage)
+    {
+        $this->ValidationError[$Control] = $ErrorMessage;
+    }
 }
