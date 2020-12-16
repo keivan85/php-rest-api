4ecc5eaf77b20d90d4a31af52809ffb699ad635f
keivan85 <keyvankarimi.kk@gmail.com>
2020-12-16 19:39:52 +0200

    fix conflict with add-apiuser-model branch

--- models/Apiusers.php
+++ models/Apiusers.php
@@ -33,10 +33,11 @@ class Apiusers
     }
 
     //Check email uniqueness
-    public function check_email() {
+    public function check_email()
+    {
         global $database;
         $this->email = trim(htmlspecialchars(strip_tags($this->email)));
-        $sql = "SELECT * FROM ". $this->table . "WHERE email = '" .$database->escape_value($this->email) ."'";
+        $sql = "SELECT * FROM " . $this->table . "WHERE email = '" . $database->escape_value($this->email) . "'";
 
         $result = $database->query($sql);
 
@@ -50,7 +51,8 @@ class Apiusers
     }
 
     //Insert/create Apiuser
-    public function create_ApiUser() {
+    public function create_ApiUser()
+    {
         //Clear data
         $this->firstname = htmlspecialchars(strip_tags($this->firstname));
 
@@ -72,15 +74,15 @@ class Apiusers
         global $database;
 
         $sql = "INSERT INTO $this->table (firstname, lastname, email, password, auth_key)
-                VALUES ('".$database->escape_value($this->firstname)."',
-                        '".$database->escape_value($this->lastname)."',
-                        '".$database->escape_value($this->email)."',
-                        '".$database->escape_value($hashed_password)."',
-                        '".$database->escape_value($auth_key)."')";
+                VALUES ('" . $database->escape_value($this->firstname) . "',
+                        '" . $database->escape_value($this->lastname) . "',
+                        '" . $database->escape_value($this->email) . "',
+                        '" . $database->escape_value($hashed_password) . "',
+                        '" . $database->escape_value($auth_key) . "')";
 
         $user_saved = $database->query($sql);
 
-        if($user_saved) {
+        if ($user_saved) {
             global $helper;
             $helper->Message = 'Registration Successful Login Here';
             $helper->Location = 'login.php';
@@ -88,7 +90,53 @@ class Apiusers
         } else {
             die('Can not save the user register later ...');
         }
+    }
 
+    //Get the Apiuser details
+    public function get_ApiuserDetails()
+    {
+        global $database;
 
+        $this->apiuser_id = intval($this->apiuser_id);
+
+        $sql = "SELECT apiuser_id, firstname, lastname, email, auth_key FROM" . $this->table . "
+        WHERE apiuser_id = '. $this->apiuser_id. '";
+
+        $result = $database->query($sql);
+        $userinfo = $database->fetch_row($result);
+
+        return $userinfo;
+    }
+
+    //Check user credentials
+    public function check_user_credentials()
+    {
+        global $database;
+
+        $this->email = trim(htmlspecialchars(strip_tags($this->email)));
+
+        $sql = "SELECT apiuser_id, firstname, lastname, email, password FROM " . $this->table . "
+        WHERE email = '" . $database->escape_value($this->email) . "'";
+
+        $result = $database->query($sql);
+
+        $user_info = $database->fetch_row($result);
+
+        if (!empty($userinfo)) {
+            //Match password
+            $hashed_password = $user_info['password'];
+
+            $password = trim(htmlspecialchars(strip_tags($this->password)));
+
+            $match_password = Bcrypt::checkPassword($password, $hashed_password);
+
+            if ($match_password) {
+                return $user_info;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 }
