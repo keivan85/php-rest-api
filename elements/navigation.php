71d2a5ca1770dfebfc7314f4899cdb6511212353
keivan85 <keyvankarimi.kk@gmail.com>
2020-12-21 20:07:11 +0200

    Add logout logic

--- elements/navigation.php
+++ elements/navigation.php
@@ -1,25 +1,42 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
+  <div class="container">
     <a class="navbar-brand" href="index.php">Article Hub</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="index">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="login">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="register">Register</a>
-      </li>
-    </ul>
-  </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+
+        <?php
+        if (!isset($_SESSION['userdata']['islogged_in'])) :
+        ?>
+          <li class="nav-item">
+            <a class="nav-link" href="login">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="register">Register</a>
+          </li>
+        <?php
+        else :
+        ?>
+          <li class="nav-item">
+            <a class="nav-link" href="profile">Profile page</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="logout">Logout</a>
+          </li>
+        <?php
+        endif;
+        ?>
+
+      </ul>
     </div>
+  </div>
 </nav>
