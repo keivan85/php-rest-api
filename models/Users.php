9be55d0d9dae2fc67d80c48521acd9bda1549b4d
keivan85 <keyvankarimi.kk@gmail.com>
2020-12-22 20:51:45 +0200

    Add Users class methods

--- models/Users.php
+++ models/Users.php
@@ -22,6 +22,34 @@ class Users {
         
     }
 
-    
+    //Validating user params
+    public function validate_param($value) {
+        if (!empty($value)) {
+            return true;
+        }
+        return false;
+    }
+
+    //Check unique email
+    public function check_unique_email() {
+        global $database;
+
+        $this->email = trim(htmlspecialchars(strip_tags($this->email)));
+
+        $sql = "SELECT user_id FROM ".$this->table." WHERE email = '".$database->escape_value($this->email)."'";
+
+        $result = $database->query($sql);
+
+        $UserInfo = $database->fetch_row($result);
+
+        if (empty($UserInfo)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
+
+$user = new Users();
 ?>
