e8049141432bbf62271abf3718d050ac6a6fab69
keivan85 <keyvankarimi.kk@gmail.com>
2020-12-22 21:02:45 +0200

    Add Users class methods

--- models/Users.php
+++ models/Users.php
@@ -49,6 +49,39 @@ class Users {
         }
     }
 
+    //Create user
+    public function create_User() {
+        //clean data
+        $this->firstname = trim(htmlspecialchars(strip_tags($this->firstname)));
+        $this->lastname = trim(htmlspecialchars(strip_tags($this->lastname)));
+        $this->email = trim(htmlspecialchars(strip_tags($this->email)));
+        $this->passwrod = trim(htmlspecialchars(strip_tags($this->passwrod)));
+
+
+        //Hash the password using Bcrypt
+        $hashed_password = Bcrypt::hashPassword($this->passwrod);
+
+        global $database;
+
+        $sql = "INSERT INTO $this->table (firstname, lastname, email, password)
+            VALUES ('".$database->escape_value($this->firstname)."',
+                    '".$database->escape_value($this->lastname)."',
+                    '".$database->escape_value($this->email)."',
+                    '".$database->escape_value($hashed_password)."')";
+
+            
+
+            try {
+                $user_saved = $database->query($sql);
+                return true;
+              }
+              //catch exception
+              catch(Exception $e) {
+                echo 'Message: ' .$e->getMessage();
+                return false;
+              }
+    } 
+
 }
 
 $user = new Users();
