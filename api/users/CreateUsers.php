7131518646309941c352f5158c3ab546045b3a2d
keivan85 <keyvankarimi.kk@gmail.com>
2020-12-24 20:37:57 +0200

    Add users Createusers api header

---
+++ api/users/CreateUsers.php
@@ -0,0 +1,21 @@
+<?php
+//headers
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+header('Access-Control-Allow-Method: POST');
+header('Access-Control-Allow-Headers: Origin, Content-type, Auth_key, Accept');
+
+include_once '../../models/Apiusers.php';
+include_once '../../models/Users.php';
+
+//Validate the request
+
+
+//Validate the content-type
+
+
+//Validate auth_key
+
+
+//Validate the param
+?>
