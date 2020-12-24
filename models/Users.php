ca4ba5c30c1c1d282eb3e35f1c795aaaf8fd14f2
keivan85 <keyvankarimi.kk@gmail.com>
2020-12-24 20:18:55 +0200

    method to get user info on users model

--- models/Users.php
+++ models/Users.php
@@ -5,7 +5,8 @@ require_once('../includes/Bcrypt.php');
 
 //Class Users
 
-class Users {
+class Users
+{
     private $table = 'users';
 
     //users properties
@@ -19,11 +20,11 @@ class Users {
 
     public function __construct()
     {
-        
     }
 
     //Validating user params
-    public function validate_param($value) {
+    public function validate_param($value)
+    {
         if (!empty($value)) {
             return true;
         }
@@ -31,12 +32,13 @@ class Users {
     }
 
     //Check unique email
-    public function check_unique_email() {
+    public function check_unique_email()
+    {
         global $database;
 
         $this->email = trim(htmlspecialchars(strip_tags($this->email)));
 
-        $sql = "SELECT user_id FROM ".$this->table." WHERE email = '".$database->escape_value($this->email)."'";
+        $sql = "SELECT user_id FROM " . $this->table . " WHERE email = '" . $database->escape_value($this->email) . "'";
 
         $result = $database->query($sql);
 
@@ -50,7 +52,8 @@ class Users {
     }
 
     //Create user
-    public function create_User() {
+    public function create_User()
+    {
         //clean data
         $this->firstname = trim(htmlspecialchars(strip_tags($this->firstname)));
         $this->lastname = trim(htmlspecialchars(strip_tags($this->lastname)));
@@ -64,25 +67,70 @@ class Users {
         global $database;
 
         $sql = "INSERT INTO $this->table (firstname, lastname, email, password)
-            VALUES ('".$database->escape_value($this->firstname)."',
-                    '".$database->escape_value($this->lastname)."',
-                    '".$database->escape_value($this->email)."',
-                    '".$database->escape_value($hashed_password)."')";
+            VALUES ('" . $database->escape_value($this->firstname) . "',
+                    '" . $database->escape_value($this->lastname) . "',
+                    '" . $database->escape_value($this->email) . "',
+                    '" . $database->escape_value($hashed_password) . "')";
 
-            
 
-            try {
-                $user_saved = $database->query($sql);
-                return true;
-              }
-              //catch exception
-              catch(Exception $e) {
-                echo 'Message: ' .$e->getMessage();
+
+        try {
+            $user_saved = $database->query($sql);
+            return true;
+        }
+        //catch exception
+        catch (Exception $e) {
+            echo 'Message: ' . $e->getMessage();
+            return false;
+        }
+    }
+
+    //Check user credentials
+
+    public function check_user_credentials()
+    {
+        global $database;
+
+        $this->email = trim(htmlspecialchars(strip_tags($this->email)));
+
+        $sql = "SELECT user_id, email, password FROM" . $this->table .
+            "WHERE email = '" . $database->escape_value($this->email) . "'";
+
+        $result = $database->query($sql);
+        $info = $database->fetch_row($result);
+
+        if (!empty($info)) {
+            $hashedPassword = $info['password'];
+
+            $passwrod = trim(htmlspecialchars(strip_tags($this->passwrod)));
+
+            $match_password = Bcrypt::checkPassword($passwrod, $hashedPassword);
+
+            if ($match_password) {
+                return $this->get_UserDetails();
+            } else {
                 return false;
-              }
-    } 
+            }
+        } else {
+            return false;
+        }
+    }
 
+    //Function to get user details
+    public function get_UserDetails()
+    {
+        global $database;
+
+        $this->email = trim(htmlspecialchars(strip_tags($this->email)));
+
+        $sql = "SELECT user_id, firstname, lastname, email FROM" . $this->table .
+            "WHERE email = '" . $database->escape_value($this->email) . "'";
+
+        $result = $database->query($sql);
+        $userInfo = $database->fetch_row($result);
+
+        return $userInfo;
+    }
 }
 
 $user = new Users();
-?>
